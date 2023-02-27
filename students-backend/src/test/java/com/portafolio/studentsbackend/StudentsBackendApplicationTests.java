b671b3fe0ba8c5466608beaa5ff60d7fdca3d410
Felipe Segovia Rodríguez <felipe.segovia.rod@gmail.com>
2023-02-27 07:25:49 -0300

    init spring boot
    se inicializa el backend

---
+++ students-backend/src/test/java/com/portafolio/studentsbackend/StudentsBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.portafolio.studentsbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudentsBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
